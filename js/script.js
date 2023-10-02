20c48517cd843e4b49656c6570daa245063c2366
lucasbarcls <104663541+lucasbarcls@users.noreply.github.com>
2023-10-02 10:41:17 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,32 +4,42 @@ const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
 const clouds = document.querySelector(".clouds");
 
+/*para que haja a animacao de pulo eh necessario adicionar a funcao jump
+na classe mario, isso sera feito na funcao jump abaixo*/
 const jump = () => {
   mario.classList.add("jump");
 
   setTimeout(() => {
+    /*remove a funcao jump da classe mario sempre que terminar a animacao (duracao da animacao eh de 500ms)
+  para que ela possa ser realizada inumeras vezes*/
     mario.classList.remove("jump");
   }, 500);
 };
 
+/*funcao para mostrar tela de game over quando o mario colidir com algum cano*/
 function gameOverScreen() {
   var gameOver = document.getElementById("game-over");
   gameOver.style.display = "block";
 }
 
 var score = 0;
+
 const loop = setInterval(() => {
-  const pipePosition = pipe.offsetLeft;
-  const marioPosition = +window
+  const pipePosition = pipe.offsetLeft; //salva a propriedade left do cano
+  const marioPosition = +window //salva a propriedade bottom do mario
     .getComputedStyle(mario)
     .bottom.replace("px", "");
-  const cloudsPosition = +window
+  const cloudsPosition = +window //salva a propriedade right das nuvens
     .getComputedStyle(clouds)
     .right.replace("px", "");
+  /*o metodo replace esta sendo chamado pois temos variaveis do tipo string, 
+  e para realizarmos calculos devemos modifica-las para variavel numerica*/
 
   score += 1;
 
-  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) { //verifica se houve colisao com o cano
+    /*caso haja colisao, a animacao do jogo eh encerrada, 
+    adiciona-se a imagem do mario colidindo e mostra a tela de game over*/
     pipe.style.animation = "none";
     pipe.style.left = `${pipePosition}px`;
 
@@ -44,7 +54,9 @@ const loop = setInterval(() => {
     clouds.style.right = `${cloudsPosition}px`;
 
     var getscore = document.getElementById("score");
-    score = score / 150;
+    score = score / 150; /*o score eh incrementado em uma unidade a cada 10ms,
+    porem queremos que o score seja aumentada toda vez que o mario pular um cano,
+    para isso dividimos seu valor por 150ms, que eh o tempo do pipe-animation*/
     getscore.textContent = "Score: " + parseInt(score);
 
     gameOverScreen();
@@ -54,7 +66,7 @@ const loop = setInterval(() => {
 }, 10);
 
 document.getElementById("play-again").addEventListener("click", function () {
-  location.reload();
+  location.reload(); //funcionalidade do botao play-again
 });
 
-document.addEventListener("keydown", jump);
+document.addEventListener("keydown", jump); //chama a funcao jump sempre que alguma tecla for pressionada
